058952026c637430b9b1512da7acfe723997caec
juamatx <60664640+juamatx@users.noreply.github.com>
2022-11-05 19:36:53 -0400

    📧 added .edu email validation
    added .edu email validation and added tests in cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,15 +3,15 @@
 // Home button is clickable and takes you to the main page
 describe("Home button", () => {
   it("clicking home button takes you to the main page", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Shareversity").click();
-    cy.url().should("eq", "https://shareversity.us/");
+    cy.url().should("eq", "http://localhost:3000/");
   });
 });
 //  Clicks on sign in button
 describe("Sign in button", () => {
   it("click sign in button", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign In").click();
   });
 });
@@ -19,7 +19,7 @@ describe("Sign in button", () => {
 // Confirms URL changed to  sign-up page
 describe("Sign in URL", () => {
   it("clicking sign in, navigates to sign-in URL", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign In").click();
     // URL changes to /sign-in
     cy.url().should("include", "/sign-in");
@@ -29,7 +29,7 @@ describe("Sign in URL", () => {
 // Clicks on "Register" button
 describe("Register button", () => {
   it("clicks register button", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Register");
   });
 });
@@ -37,7 +37,7 @@ describe("Register button", () => {
 // Confirms URL changed to  sign-up page
 describe("Check register page URL", () => {
   it("clicking register, navigates to a new URL", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Register").click();
     // URL changes to /sign-in
     cy.url().should("include", "/sign-up");
@@ -47,7 +47,7 @@ describe("Check register page URL", () => {
 //  Clicks on sign up button
 describe("Sign up button", () => {
   it("click sign up button", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign Up").click();
   });
 });
@@ -55,7 +55,7 @@ describe("Sign up button", () => {
 // Confirms URL changed to  sign-up page
 describe("Sign up URL", () => {
   it("clicking sign up, navigates to sign-up URL", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign Up").click();
     // URL changes to /sign-up
     cy.url().should("include", "/sign-up");
@@ -65,7 +65,7 @@ describe("Sign up URL", () => {
 // Already have an account link works
 describe("Already have an account", () => {
   it("clicking sign up, navigates to sign-up URL", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign Up").click();
     // URL changes to /sign-up
     cy.url().should("include", "/sign-up");
@@ -77,7 +77,7 @@ describe("Already have an account", () => {
 //Not a member yet? Sign Up here
 describe("Not a member link", () => {
   it("not a member link takes you to sign-up page", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign In").click();
     cy.url().should("include", "sign-in");
     cy.contains("Sign Up here").click();
@@ -88,7 +88,7 @@ describe("Not a member link", () => {
 // Sign-up fake user
 describe("Sign up user", () => {
   it("signing up form", () => {
-    cy.visit("https://shareversity.us");
+    cy.visit("http://localhost:3000");
     cy.contains("Sign Up").click();
     // URL changes to /sign-up
     cy.url().should("include", "/sign-up");
@@ -97,5 +97,38 @@ describe("Sign up user", () => {
     cy.get("input[name=lastName]").type("Doe");
     cy.get("input[name=email]").type("jdoe@harvard.edu");
     cy.get("input[name=password]").type("averylongpwthatneedstoworkwherewewe");
+  });
+});
+
+// A proper email is entered
+describe("Invalid email warning", () => {
+  it("a valid email is entered and user is warned", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Sign Up").click();
+    // URL changes to /sign-up
+    cy.url().should("include", "/sign-up");
+    // entered a invalid email to test
+    cy.get("input[name=email]").type("jdoeharvard.com");
+    //click outside the form
+    cy.get("body").click(0, 0);
+    // user gets warned
+    cy.contains("Invalid email");
+  });
+});
+
+// An edu email is entered
+describe("Non .edu email warning", () => {
+  it("a non-edu emails is entered and user is warned", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Sign Up").click();
+    // URL changes to /sign-up
+    cy.url().should("include", "/sign-up");
+    cy.get("input[name=firstName]").type("John");
+    cy.get("input[name=lastName]").type("Doe");
+    // entered a non-edu email to test
+    cy.get("input[name=email]").type("jdoe@harvard.com");
+    cy.get("body").click(0, 0);
+    // user gets warned
+    cy.contains(".edu email required");
   });
 });
